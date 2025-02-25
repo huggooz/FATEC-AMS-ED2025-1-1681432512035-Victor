b9b56fffe775a0630bde29ea7e40b0a5c2bb97ba
Victor Hugo <vh.goncalves2007@gmail.com>
2025-02-24 21:24:32 -0300

    entrega/atividade-s1-b1-1

---
+++ AMS-ED-2025-Entregas-S1-S2/AMS-ED-2025-Entregas-S1-S2/AMS-ED-2025-Entregas-S1/AMS-ED-2025-Entregas-S1-B1/AMS-ED-2025-Entregas-S1-B1-Atividade1/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+// Função para aplicar o desconto usando ponteiros
+void aplicarDesconto(float *preco, float desconto, int *tipoDesconto, float *valorDesconto) {
+    if (*tipoDesconto == 1) {
+        *valorDesconto = *preco * (desconto / 100);
+        *preco -= *valorDesconto; // Desconto em porcentagem
+    } else {
+        *valorDesconto = desconto;
+        *preco -= *valorDesconto; // Desconto em reais
+    }
+}
+
+int main() {
+    float preco, desconto, valorDesconto = 0;
+    int aplicar, tipoDesconto;
+    
+    // Obtendo os dados do usuário
+    printf("Informe o preço do produto: ");
+    scanf("%f", &preco);
+    
+    printf("Deseja aplicar algum desconto? (1 - Sim, 0 - Não): ");
+    scanf("%d", &aplicar);
+    
+    if (aplicar) {
+        printf("Qual o tipo de desconto (1 - Percentual, 2 - Em reais): ");
+        scanf("%d", &tipoDesconto);
+        
+        printf("Informe o valor do desconto: ");
+        scanf("%f", &desconto);
+        
+        // Aplicando desconto
+        aplicarDesconto(&preco, desconto, &tipoDesconto, &valorDesconto);
+    }
+    
+    // Exibe os resultados
+    printf("\nValor do desconto: R$ %.2f\n", valorDesconto);
+    printf("Preço final: R$ %.2f\n", preco);
+    
+    return 0;
+}
