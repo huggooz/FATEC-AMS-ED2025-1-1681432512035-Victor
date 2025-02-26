388e2e176e8318282c6c8be39b5d5af85f09e75c
Victor Hugo Gonçalves <vh.goncalves2007@gmail.com>
2025-02-26 15:05:37 -0300

    entrega/atividade-s1-b1-1

--- AMS-ED-2025-Entregas-S1-S2/AMS-ED-2025-Entregas-S1-S2/AMS-ED-2025-Entregas-S1/AMS-ED-2025-Entregas-S1-B1/AMS-ED-2025-Entregas-S1-B1-Atividade1/main.c
+++ AMS-ED-2025-Entregas-S1-S2/AMS-ED-2025-Entregas-S1-S2/AMS-ED-2025-Entregas-S1/AMS-ED-2025-Entregas-S1-B1/AMS-ED-2025-Entregas-S1-B1-Atividade1/main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-void aplicarDesconto(float *preco, float desconto, int *tipoDesconto, float *valorDesconto) {
+void aplicarDesconto(float *preco, float desconto, int *tipoDesconto, float *valorDesconto, float *preco_final) {
     if (*tipoDesconto == 1) {
         *valorDesconto = *preco * (desconto / 100);
-        *preco -= *valorDesconto; 
+        *preco_final = *preco - *valorDesconto; 
     } else {
         *valorDesconto = desconto;
-        *preco -= *valorDesconto; 
+        *preco_final = *preco - *valorDesconto; 
     }
 }
 
 int main() {
-    float preco, desconto, valorDesconto = 0;
+    float preco, desconto, valorDesconto = 0, preco_final;
     int aplicar, tipoDesconto;
     
     printf("Informe o preço do produto: ");
@@ -20,18 +20,23 @@ int main() {
     printf("Deseja aplicar algum desconto? (1 - Sim, 0 - Não): ");
     scanf("%d", &aplicar);
     
-    if (aplicar) {
+    if (aplicar == 1) {
         printf("Qual o tipo de desconto (1 - Percentual, 2 - Em reais): ");
         scanf("%d", &tipoDesconto);
         
         printf("Informe o valor do desconto: ");
         scanf("%f", &desconto);
         
-        aplicarDesconto(&preco, desconto, &tipoDesconto, &valorDesconto);
+        aplicarDesconto(&preco, desconto, &tipoDesconto, &valorDesconto, &preco_final);
+    }
+    else{
+        
+        aplicarDesconto(&preco, desconto, &tipoDesconto, &valorDesconto, &preco_final);
     }
     
+    printf("\nPreço original: R$ %.2f", preco);
     printf("\nValor do desconto: R$ %.2f\n", valorDesconto);
-    printf("Preço final: R$ %.2f\n", preco);
+    printf("Preço final: R$ %.2f\n", preco_final);
     
     return 0;
 }
