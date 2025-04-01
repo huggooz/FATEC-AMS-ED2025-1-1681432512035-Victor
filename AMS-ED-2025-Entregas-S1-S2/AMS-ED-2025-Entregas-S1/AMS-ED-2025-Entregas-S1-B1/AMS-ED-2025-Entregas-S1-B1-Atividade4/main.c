e82d009f9ab7aaaadd59e157f25d4ffb9784197f
Victor Hugo Gonçalves <vh.goncalves2007@gmail.com>
2025-04-01 16:16:33 -0300

    Entrega S1-B1-Atividade 4

--- AMS-ED-2025-Entregas-S1-S2/AMS-ED-2025-Entregas-S1/AMS-ED-2025-Entregas-S1-B1/AMS-ED-2025-Entregas-S1-B1-Atividade4/main.c
+++ AMS-ED-2025-Entregas-S1-S2/AMS-ED-2025-Entregas-S1/AMS-ED-2025-Entregas-S1-B1/AMS-ED-2025-Entregas-S1-B1-Atividade4/main.c
@@ -22,122 +22,126 @@
 #include <string.h>
 
 #define MAX_SIZE 100
-#define MAX_EXPR_LEN 400
 
 typedef struct {
-    char expressao[MAX_SIZE][MAX_EXPR_LEN]; 
-    double data[MAX_SIZE]; 
+    double data[MAX_SIZE];
+    char expr[MAX_SIZE][50];
     int top;
 } Pilha;
 
-void criar_pilha(Pilha *p) {
+Pilha* criar_pilha() {
+    Pilha* p = (Pilha*)malloc(sizeof(Pilha));
     p->top = -1;
+    return p;
 }
 
-int vazia(Pilha *p) {
+int vazia(Pilha* p) {
     return p->top == -1;
 }
 
-int cheia(Pilha *p) {
+int cheia(Pilha* p) {
     return p->top == MAX_SIZE - 1;
 }
 
-void push(Pilha *p, double dado, const char *expr) {
+void push(Pilha* p, double valor, const char* expr) {
     if (cheia(p)) {
         printf("Erro: Pilha cheia!\n");
         return;
     }
-    p->data[++p->top] = dado;
-    strncpy(p->expressao[p->top], expr, MAX_EXPR_LEN - 1);
-    p->expressao[p->top][MAX_EXPR_LEN - 1] = '\0'; 
+    p->top++;
+    p->data[p->top] = valor;
+    strcpy(p->expr[p->top], expr);
 }
 
-double pop(Pilha *p, char *expr) {
+double pop(Pilha* p, char* expr) {
     if (vazia(p)) {
         printf("Erro: Pilha vazia!\n");
-        return 0;
+        exit(1);
     }
-    strncpy(expr, p->expressao[p->top], MAX_EXPR_LEN - 1);
-    expr[MAX_EXPR_LEN - 1] = '\0'; 
+    strcpy(expr, p->expr[p->top]);
     return p->data[p->top--];
 }
 
-void calcular_rpn(char *entrada) {
-    Pilha pilha;
-    criar_pilha(&pilha);
-    char *tokens[MAX_SIZE];
-    int count = 0;
-
-    char *token = strtok(entrada, " ");
-    while (token != NULL) {
-        tokens[count++] = token;
-        token = strtok(NULL, " ");
+double top(Pilha* p, char* expr) {
+    if (vazia(p)) {
+        printf("Erro: Pilha vazia!\n");
+        exit(1);
     }
+    strcpy(expr, p->expr[p->top]);
+    return p->data[p->top];
+}
 
-    for (int i = 0; i < count; i++) {
-        token = tokens[i];
-
+void calcular_RPN(char* expressao) {
+    Pilha* pilha = criar_pilha();
+    char* token = strtok(expressao, " ");
+    
+    while (token != NULL) {
         if (isdigit(token[0]) || (token[0] == '-' && isdigit(token[1]))) {
-            push(&pilha, atof(token), token);
+            double num = atof(token);
+            char temp[20];
+            sprintf(temp, "%.2f", num);
+            push(pilha, num, temp);
         } else {
-            if (pilha.top < 1) {
-                printf("Erro: Expressao invalida!\n");
+            if (pilha->top < 1) {
+                printf("Erro: Expressão inválida!\n");
                 return;
             }
-            char expr1[MAX_EXPR_LEN], expr2[MAX_EXPR_LEN];
-            double b = pop(&pilha, expr2);
-            double a = pop(&pilha, expr1);
-
-            char novaExpr[MAX_EXPR_LEN];
-            if (snprintf(novaExpr, sizeof(novaExpr), "(%s %c %s)", expr1, token[0], expr2) >= MAX_EXPR_LEN) {
-                printf("Erro: Expressao muito longa!\n");
-                return;
-            }
-
-            switch (token[0]) {
-                case '+': push(&pilha, a + b, novaExpr); break;
-                case '-': push(&pilha, a - b, novaExpr); break;
-                case '*': push(&pilha, a * b, novaExpr); break;
+            char exprX[50], exprY[50];
+            double y = pop(pilha, exprY);
+            double x = pop(pilha, exprX);
+            double t;
+            char z = token[0];
+            char novaExpr[150];
+            
+            switch (z) {
+                case '+': t = x + y; break;
+                case '-': t = x - y; break;
+                case '*': t = x * y; break;
                 case '/':
-                    if (b == 0) {
-                        printf("Erro: Divisao por zero!\n");
+                    if (y == 0) {
+                        printf("Erro: Divisão por zero!\n");
                         return;
                     }
-                    push(&pilha, a / b, novaExpr);
+                    t = x / y;
                     break;
                 default:
-                    printf("Erro: Operador invalido!\n");
+                    printf("Erro: Operador inválido!\n");
                     return;
             }
+            
+            snprintf(novaExpr, sizeof(novaExpr), "(%s %c %s)", exprX, z, exprY);
+            push(pilha, t, novaExpr);
         }
+        token = strtok(NULL, " ");
     }
-
-    if (pilha.top != 0) {
-        printf("Erro: Expressao invalida!\n");
+    
+    if (pilha->top != 0) {
+        printf("Erro: Expressão inválida!\n");
         return;
     }
-
-    char resultadoExpr[MAX_EXPR_LEN];
-    double resultado = pop(&pilha, resultadoExpr);
-    printf("Expressao algébrica: %s\n", resultadoExpr);
-    printf("Resultado: %.2f\n", resultado);
+    
+    char resultadoExpr[100];
+    double resultadoFinal = pop(pilha, resultadoExpr);
+    printf("Resultado: %.2f\n", resultadoFinal);
+    printf("Expressão algebrica: %s\n", resultadoExpr);
+    
+    free(pilha);
 }
 
 int main() {
-    char expressao[256];
+    char expressao[MAX_SIZE];
 
     while (1) {
-        printf("Digite a expressao RPN (ou 'sair' para encerrar): ");
-        fgets(expressao, sizeof(expressao), stdin);
+        printf("\nDigite a expressão RPN (ou 'sair' para encerrar): ");
+        fgets(expressao, MAX_SIZE, stdin);
         expressao[strcspn(expressao, "\n")] = 0;
 
         if (strcmp(expressao, "sair") == 0) {
-            printf("Encerrando a calculadora...\n");
+            printf("Encerrando o programa...\n");
             break;
         }
 
-        calcular_rpn(expressao);
-        printf("\n");
+        calcular_RPN(expressao);
     }
 
     return 0;
