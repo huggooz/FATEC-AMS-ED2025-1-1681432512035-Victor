0465d7fb00106d9ea38c9eaf7d3cb76e9fe9b297
Victor Hugo Gonçalves <vh.goncalves2007@gmail.com>
2025-05-06 17:57:51 -0300

    Entrega-S1-B2-Atividade2

--- AMS-ED-2025-Entregas-S1-S2/AMS-ED-2025-Entregas-S1/AMS-ED-2025-Entregas-S1-B2/AMS-ED-2025-Entregas-S1-B2-Atividade2/main.c
+++ AMS-ED-2025-Entregas-S1-S2/AMS-ED-2025-Entregas-S1/AMS-ED-2025-Entregas-S1-B2/AMS-ED-2025-Entregas-S1-B2-Atividade2/main.c
@@ -1,63 +1,56 @@
-/*-----------------------------------------------------------------------------------*/
-/*   FATEC-São Caetano do Sul                 Estrutura de Dados                     */
-/*                                                                                   */
-/*                                      Id da Atividade: Atividade B2-2-Bubblesort   */
-/*                                                                                   */
-/*   Objetivo: Programa em C que lê 5 números inteiros, os ordena usando o           */
-/*             algoritmo Bubble Sort (com estrutura de repetição while) e exibe o    */
-/*             vetor antes e depois da ordenação. A ordenação é feita comparando     */
-/*             pares de elementos vizinhos e trocando-os se estiverem fora de ordem. */                                                   */
-/*                                                                                   */
-/*                                  Autor: Victor Hugo Gonçalves                     */
-/*                                                                  Data:06/05/2025  */
-/*-----------------------------------------------------------------------------------*/
-
+/*----------------------------------------------------------------------------------/
+/*   FATEC-São Caetano do Sul                 Estrutura de Dados                    /
+/                         Id da Atividade: S1-B2-2                                  /
+/             Objetivo: << BubbleSort >>                                            /
+/                                                                                   /
+/                                  Autor: Fernando Alcantara D'Avila                /
+/                                                                  Data:06/05/2025  /
+/----------------------------------------------------------------------------------*/
 #include <stdio.h>
 
-void bubbleSort(int vet[]) {
-    int i, n = 5, cont = 0;
+void bubbleSort(int array[], int N) {
+    int i, j, temp;
 
-    while (cont < n) {
-        i = 0;
-        while (i < n - 1 - cont) {
-            if (vet[i] > vet[i + 1]) {
-                int temp = vet[i];
-                vet[i] = vet[i + 1];
-                vet[i + 1] = temp;
+    for (i = 0; i < N - 1; i++) {
+        for (j = 0; j < N - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
             }
-            i++;
         }
-        cont++;
     }
+}
+
+void printArray(int array[], int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", array[i]);
+    printf("\n");
 }
 
 int main() {
-    int vet[5];
-    int i;
+    int N;
 
-    printf("Digite 5 números inteiros: (ex: 3 2 4 1 5)\n");
-    i = 0;
-    while (i < 5) {
-        scanf("%d", &vet[i]);
-        i++;
+
+    printf("Digite o número de elementos: ");
+    scanf("%d", &N);
+
+    int array[N];
+
+
+    printf("Digite os %d elementos:\n", N);
+    for (int i = 0; i < N; i++) {
+        printf("Elemento %d: ", i + 1);
+        scanf("%d", &array[i]);
     }
 
-    printf("\nAntes da ordenação:\n");
-    i = 0;
-    while (i < 5) {
-        printf("%d ", vet[i]);
-        i++;
-    }
+    printf("\nVetor original: ");
+    printArray(array, N);
 
-    bubbleSort(vet);
+    bubbleSort(array, N);
 
-    printf("\nDepois da ordenação:\n");
-    i = 0;
-    while (i < 5) {
-        printf("%d ", vet[i]);
-        i++;
-    }
+    printf("Vetor ordenado: ");
+    printArray(array, N);
 
     return 0;
 }
-
